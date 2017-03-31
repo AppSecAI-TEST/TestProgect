21fbfeafcd5fc5b11cb8be5c917eadd9ab08e47e
llong0807@163.com <ll19881990>
2017-03-31 17:32:21 +0800

    other info

---
+++ app/src/main/java/com/tianniu/up/testprogect/BaseFragment.java
@@ -0,0 +1,73 @@
+package com.tianniu.up.testprogect;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.util.SparseArrayCompat;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Administrator on 2017/3/31 0031.
+ */
+
+public abstract class BaseFragment extends Fragment implements View.OnClickListener {
+
+    private boolean isVisible = false;
+    private boolean isInitView = false;
+    private boolean isFirstLoad = false;
+
+    private View convertView;
+    private SparseArrayCompat<View> mViews = new SparseArrayCompat<>();
+    public abstract int getLayoutId();
+    public abstract void initView();
+    public abstract void initListener();
+    public abstract void initData();
+    public abstract void processClick(View view);
+
+    @Override
+    public void setUserVisibleHint(boolean isVisibleToUser) {
+        super.setUserVisibleHint(isVisibleToUser);
+        if (isVisibleToUser){
+            isVisible = true;
+            lazyload();
+        }else {
+            isVisible = false;
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        convertView = inflater.inflate(getLayoutId(),container,false);
+        initView();
+        isInitView = true;
+        lazyload();
+        return convertView;
+    }
+
+    private void lazyload() {
+        if (isFirstLoad || isVisible || isInitView){
+            initListener();
+            initData();
+            isFirstLoad = false;
+        }
+    }
+
+    public <E extends View >E findView(int viewid){
+        if (convertView != null){
+            E view = (E)mViews.get(viewid);
+            if (view == null){
+                view =(E)convertView.findViewById(viewid);
+                mViews.put(viewid,view);
+            }
+            return view;
+        }
+        return null;
+    }
+
+    public <E extends View>void  setOnclick(E view){
+        view.setOnClickListener(this);
+    }
+
+}
---
+++ model/src/main/java/com/tianniu/up/model/JsonTag.java
@@ -0,0 +1,27 @@
+package com.tianniu.up.model;
+
+/**
+ * Created by Administrator on 2017/3/31 0031.
+ */
+
+public class JsonTag {
+    public static final String CARGO_ID = "cargoId";
+    public static final String CARGO_DESCRIBE = "cargoDescribe";
+    public static final String START_POINT = "startPoint";
+    public static final String DEST_POINT = "destinationPoint";
+    public static final String START_LONLAT = "startLonLat";
+    public static final String DEST_LONLAT = "destLonLat";
+
+    public static final String CARGO_MILEAGE = "cargoMileage";
+    public static final String CARGO_PUBLISHER_NIKENAME = "cargoPublisherNikeName";
+    public static final String CARGO_PUBLISHER_ICON_URL = "cargoPublisherIconUrl";
+
+    public static final String CARGO_PUBLISHE_TIME = "cargoPublishTime";
+
+    public static final String CARGO_TURCK_LOADING_TIMA = "cargoTurckLoadingTime";
+    public static final String CARGO_TYPE = "cargoType";
+    public static final String CARGO_WEIGHT = "cargoWeight";
+    public static final String CARGO_AREA = "cargoArea";
+    public static final String CARGO_REMARK ="cargoRemark";
+
+}
