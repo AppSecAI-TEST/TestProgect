2d16aeaed11f6f034cb86b6bdd601ccc1e0747a0
llong0807@163.com <ll19881990>
2017-04-05 11:50:12 +0800

    change structure

--- model/src/main/java/com/tianniu/up/model/bean/io/CargoMsgBreviary.java
+++
@@ -1,9 +0,0 @@
-package com.tianniu.up.model.bean.io;
-
-/**
- * Created by Administrator on 2017/3/31 0031.
- */
-
-public class CargoMsgBreviary extends CargoMsgInfo {
-
-}
---
+++ vehiclemaster/src/main/java/com/tianniu/up/vehiclemaster/model/io/CargoMsgBreviary.java
@@ -0,0 +1,9 @@
+package com.tianniu.up.vehiclemaster.model.io;
+
+/**
+ * Created by Administrator on 2017/3/31 0031.
+ */
+
+public class CargoMsgBreviary extends CargoMsgInfo {
+
+}
